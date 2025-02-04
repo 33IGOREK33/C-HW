da1ad355aaadd53a67bd00f04d9bf266f9893f25
33IGOREK33 <igor.yakovlev.2222@gmail.com>
2025-02-03 19:05:34 -0500

    task 1 is done

---
+++ task2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int rows, columns;
+    rows = 10;
+    columns = 10;
+    char zeroChar = '0';
+
+    for (int x = 0;  x < rows; x++)
+    {
+        for (int y = 0; y < columns; y++)
+        {
+            std::cout << zeroChar;
+        }
+        std::cout << std::endl;
+    }
+    
+}
